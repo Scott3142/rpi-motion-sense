c8336ad373a5626a8200911d5fda3c95b8f9fbb2
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-25 14:50:24 +0100

    Updates file path

--- motion_sense.py
+++ motion_sense.py
@@ -10,6 +10,6 @@ camera.rotation = 180
 while True:
     motion.wait_for_motion()
     dt_string = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
-    filename = "/home/pi/pir/pic" + dt_string + ".jpg"
+    filename = "pic" + dt_string + ".jpg"
     camera.capture(filename)
     sleep(2)
